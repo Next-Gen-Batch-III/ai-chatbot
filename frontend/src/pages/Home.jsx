8ea520520f51ba30afa530ced0cfbe8d2aa1149a
NgetSokunkanha <ngetsokunkanha1@gmail.com>
2026-08-05 23:14:15 +0700

    implement the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,28 @@
-import {useState} from 'react';
+import { useUser } from '@clerk/clerk-react';
 
 import Sidebar from '../components/layout/Sidebar';
 import ChatInput from '../components/chat/ChatInput';
 import QuickActions from '../components/chat/QuickActions';
 import WelcomeMessage from '../components/chat/WelcomeMessage';
 
-export default function Home({ user = { name: "Guest"}, onSend }) {
+export default function Home({ onSend }) {
+    const { user } = useUser();
+
     const handleQuickAction = (label) => {
         onSend?.(label);
     };
+
+    const userData = {
+        name: user?.firstName,
+        email: user?.emailAddresses?.[0]?.emailAddress,
+        imageUrl: user?.imageUrl,
+    };
+
     return (
         <div className="flex h-full flex-1 flex-col items-center justify-center bg-white px-6">
-            <Sidebar user={user} />
+            <Sidebar user={userData} />
             <main className="chat">
-                <WelcomeMessage name={user.name} />
+                <WelcomeMessage name={userData.name} />
                 <ChatInput onSend={onSend} />
                 <QuickActions onAction={handleQuickAction} />
             </main>
